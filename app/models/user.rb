c027281681c57ed25806fda1c610acc2c05dde6b
takahashi0212 <rt6965@yahoo.co.jp>
2020-08-28 18:44:46 +0900

    新規登録/ログイン/ログアウト実装

---
+++ app/models/user.rb
@@ -0,0 +1,34 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+
+         validates :name, presence: true, length: { maximum: 20 }
+         validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+         validates :password, presence: true, length: {minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+       with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+          validates :family_name
+          validates :first_name
+          validates :family_name_kana
+          validates :first_name_kana
+       end
+          validates :birth_day, presence: true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
