e4a8b49242cc56f7ad5ddbfe3895ef67134d1fd4
takahashi0212 <rt6965@yahoo.co.jp>
2020-09-10 20:02:11 +0900

    購入機能の実装、環境変数の設定

---
+++ app/controllers/buyers_controller.rb
@@ -0,0 +1,34 @@
+class BuyersController < ApplicationController
+
+  def index
+    @product = Product.find(params[:product_id])
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @buyer = BuyerForm.new(buyer_params)
+    if @buyer.valid?
+      pay_product
+      @buyer.save
+      return redirect_to root_path
+    else
+      @product = Product.find(params[:product_id])
+      render 'index'
+    end
+  end
+
+  private
+  def buyer_params
+    params.permit(:price, :post_code, :zone_id, :city, :adress, :building, :phone_number, :token, :product_id).merge(user_id: current_user.id)
+  end
+
+  def pay_product
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @product.price,
+      card: buyer_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+end
